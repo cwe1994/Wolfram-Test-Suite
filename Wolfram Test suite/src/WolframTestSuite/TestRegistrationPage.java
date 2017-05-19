d55b0516002daa36a390ee8fa6779a9ab677bc35
cwe1994 <cwe9416@gmail.com>
2017-05-19 15:40:45 -0500

    Completed suite and post test
    This includes the test results and the test suite created for the steps. the suite unfortunately doesn't seem to work as it should.

--- Wolfram Test suite/src/WolframTestSuite/TestRegistrationPage.java
+++ Wolfram Test suite/src/WolframTestSuite/TestRegistrationPage.java
@@ -3,25 +3,46 @@ package WolframTestSuite;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import java.util.NoSuchElementException;
+import java.util.concurrent.TimeUnit;
 
 public class TestRegistrationPage {
 	private WebDriver driver;
-	
-	public TestRegistrationPage(WebDriver driver){
+
+	public TestRegistrationPage(WebDriver driver) {
 		this.driver = driver;
-		if (!"Sign UP - Wolfram Programming Cloud".equals(this.driver.getTitle()))
-		{//checks the page title
+		if (!"Sign UP - Wolfram Programming Cloud".equals(this.driver.getTitle())) {// checks
+																					// the
+																					// page
+																					// title
 			throw new NoSuchElementException("This is not the login page");
 		}
-		
-		//verifies that the required field are there
-		String[] elements = {"email", "firstname", "lastname",  "password", "password2", "signIn"};
-		for(int i = 0; i <elements.length; i++){
-			if(driver.findElements(By.id(elements[i])).size() == 0)
-			{
+
+		// verifies that the required field are there
+		String[] elements = { "email", "firstname", "lastname", "password", "password2", "signIn" };
+		for (int i = 0; i < elements.length; i++) {
+			if (driver.findElements(By.id(elements[i])).size() == 0) {
 				throw new NoSuchElementException(elements[i] + "field not present");
 			}
 		}
+	}
+
+	// enters the account info
+	// returns homescreen for the newly registered user
+	public TestHomescreen register(String[] info) {
+		String[] elements = { "email", "firstname", "lastname", "password" };
+
+		for (int i = 0; i < elements.length; i++) {
+			driver.findElement(By.id(elements[i])).clear();
+			driver.findElement(By.id(elements[i])).sendKeys(info[i]);
+		}
+		driver.findElement(By.id("password2")).clear();
+		driver.findElement(By.id("password2")).sendKeys(info[info.length - 1]);
+		driver.findElement(By.id("signIn")).click();
+
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+		// update if next page is no longer Homescreen
+		return new TestHomescreen(driver);
 	}
 
 }
--- Wolfram Test suite/src/WolframTestSuite/WolframCloudTestSuite.java
+++ Wolfram Test suite/src/WolframTestSuite/WolframCloudTestSuite.java
@@ -15,6 +15,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.concurrent.TimeUnit;
+import java.io.File;
 import java.util.NoSuchElementException;
 import java.util.Random;
 
@@ -29,6 +30,8 @@ public class WolframCloudTestSuite {
 	
 	@BeforeSuite
 	public void setUp() throws Exception{
+		File file = new File("C:/Selenium/chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
 		driver = new ChromeDriver();
 		baseURL = "https://www.wolframcloud.com/";
 		System.out.println("Before Suite.");
@@ -44,8 +47,9 @@ public class WolframCloudTestSuite {
 	
 	//Tests the launch of the Wolfram Cloud Site
 	@Test(groups={"functest", "usrexptest"}, priority=1)
-	public void testLaunch(){
+	public void suiteLaunch(){
 		driver.get(baseURL); //waits for page to load
+		System.out.println("URL gotten.");
 		try{
 			this.myCloudPage = new TestCloudPage(driver);
 		}catch(NoSuchElementException e){
@@ -69,6 +73,89 @@ public class WolframCloudTestSuite {
 	}
 	
 	//Tests the account registration page
-	@Test(groups={"functest")
+	@Test(groups={"functest",  "usrexptest"}, priority=3)
+	public void tryRegistration(){
+		try{
+			this.myRegistrationPage = myLoginPage.getRegistrationPage();
+		}
+		catch(NoSuchElementException e){
+			System.out.println("Registration page failed to load");
+		}
+		
+		Assert.assertTrue(driver.getTitle().contains("Sign Up - Wolfram Programming Cloud"));
+		
+		//uses random number generator to create login name and password
+		Random rand = new Random();
+		//six character password is required
+		int randInt = rand.nextInt(899999) + 100000;
+		String email = Integer.toString(randInt) + "@test.com";
+		String firstname = "test";
+		String lastname = "suite";
+		String password = Integer.toString(randInt);
+		
+		String[] info = {email, firstname, lastname, password};
+		
+		//checks that various invalid input produces errors
+		String[] info2 = new String[4];
+		System.arraycopy(info,  0,  info2,  0,  info.length);
+		
+		//missing entries should generate errors
+		for(int i = 0; i < 4; i++){
+			//replace value with empty string
+			info2[i] = info2[i].replace(info[i],  "");
+			myRegistrationPage.register(info2);
+			Assert.assertTrue(driver.findElement(By.className("error")).isDisplayed());
+			System.arraycopy(info,  0,  info2,  0,  info.length);
+		}
+		
+		//less than 6 characters for password should generate an error
+		info2[info.length-1] = info2[info.length-1].replace(info[info.length-1], Integer.toString(randInt/10));
+		myRegistrationPage.register(info2);
+		Assert.assertTrue(driver.findElement(By.className("error")).isDisplayed());
+		
+		//check the valid entries
+		try{
+			this.myHomescreen = myRegistrationPage.register(info);
+		}
+		catch(NoSuchElementException e){
+			System.out.println("Could not register properly");
+		}
+		//check that the form entries created account successfully
+		Assert.assertEquals(driver.getTitle(),"Home - Wolfram Programming Cloud");
+	}
+	
+	//tests generation of Homescreen and contents
+	@Test(groups={"usrexptest"}, priority=4)
+	public void tryHomescreen(){
+		//Double check we are on homescreen
+		Assert.assertEquals(driver.getTitle(), "Home - Wolfram Programming Cloud");
+		
+		//checks for upload button
+		//checks for other elements similarly
+		Assert.assertTrue(driver.findElements(By.id("uploadBtn")).size() > 0);
+	}
+	
+	//tests the notebook document interface
+	@Test(groups={"functest", "usrexptest"}, priority=5)
+	public void tryNotebook(){
+		//specify what type of document to create
+		//can change to other DocTypes for more tests
+		String newDocType = "Notebook";
+		
+		try{
+			this.myNotebook = myHomescreen.createNb(newDocType);
+		}
+		catch(NoSuchElementException e){
+			System.out.println(newDocType + "cannot be created");
+		}
+		
+		Assert.assertTrue(driver.getTitle().contains("(unnamed) - Wolfram Programming Cloud"));
+		
+		//checks that the name field reads "(unnamed)"
+		Assert.assertEquals(myNotebook.getNameInputVal(), "(unnamed)");
+		
+		//checks that the value after click has the right extension
+		Assert.assertEquals(myNotebook.getNameInputExt(), "."+ TestDocType.valueOf(newDocType).getId());
+	}
 	
 }
